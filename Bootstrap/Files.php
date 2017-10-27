05a4890e3e9f3e2f3f7301e4d9147e4c02befe99
Sebastian Wieland <swieland@conexco.com>
2017-10-27 17:42:44 +0200

    Reverted Remove, kept refactor

--- Bootstrap/Files.php
+++ Bootstrap/Files.php
@@ -1,30 +1,26 @@
 <?php
 
 /**
- *   Shopware Bootstrap Theme - helps you build fast, robust, and adaptable responsive shopware themes
- *   Copyright (C) 2017, conexco UG (haftungsbeschränkt) & Co. KG
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+*   Shopware Bootstrap Theme - helps you build fast, robust, and adaptable responsive shopware themes
+*   Copyright (C) 2017, conexco UG (haftungsbeschränkt) & Co. KG
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 namespace SwfBootstrapTheme\Bootstrap;
 
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
 use Shopware\Components\Plugin\Context\UpdateContext;
-use SplFileInfo;
-use function version_compare;
 
 class Files extends SetupBase
 {
@@ -56,34 +52,9 @@ class Files extends SetupBase
         }
     }
 
-    private function v1_0_1_removeShopwareJsFiles()
-    {
-        $shopwareJsFilePath = $this->pluginDir . '/Resources/Themes/Frontend/BootstrapBare/frontend/_public/vendors/js/shopware/';
-
-        $files = new RecursiveIteratorIterator(
-            new RecursiveDirectoryIterator($shopwareJsFilePath, RecursiveDirectoryIterator::SKIP_DOTS),
-            RecursiveIteratorIterator::CHILD_FIRST
-        );
-
-        /** @var SplFileInfo $file */
-        foreach ($files as $file) {
-            if ($file->isDir()) {
-                rmdir($file->getRealPath());
-            } else {
-                unlink($file->getRealPath());
-            }
-        }
-
-        rmdir($shopwareJsFilePath);
-    }
-
 
     public function update(UpdateContext $context)
     {
         $this->deleteOldFiles();
-
-        if (version_compare($context->getCurrentVersion(), '1.0.2', '<=')) {
-            $this->v1_0_1_removeShopwareJsFiles();
-        }
     }
 }
